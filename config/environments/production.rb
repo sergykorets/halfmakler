afe765682c0d6b5b940ecc15785dc3527a55a371
gokart <sergykoretsfsp@gmail.com>
2017-10-10 23:21:08 +0300

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://halfmakler.herokuapp.com/', port: 80 }
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: 'plain',
+    user_name: 'sergykoretsfsp@gmail.com',
+    password: '3441_Edinorog'
+  }
 end
